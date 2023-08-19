fc2009c3d8557a46de8297dd89a37c4365b446bf
Andrey <andreypremiere@mail.ru>
2023-08-19 19:33:15 +0700

    Создан базовый шаблон html страницы

--- posterSFU/manageevents/urls.py
+++ posterSFU/manageevents/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import index, news
 
 # здесь прописываем все маршруты текущего приложения
 urlpatterns = [
-    path('', index)
+    path('', index),
+    path('news/', news)
 ]
--- posterSFU/manageevents/views.py
+++ posterSFU/manageevents/views.py
@@ -1,11 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import ListUniversity
 
 
 # Обязательный параметр request.
 # Функцию представления нужно связывать с соответствующим url-адресом.
 def index(request):
-    return HttpResponse('Страница приложения')
+    # В эту переменную сохраняем записи из базы данных модели ListUniversity (вернет список?)
+    list_university = ListUniversity.objects.all()
+    # Параметры и переменные, которые передаются на html страницу, передаются третьим аргументом в виде словаря
+    return render(request, 'manageevents/index.html', {'title': 'Главная страница',
+                                                       'list_university': list_university})
 
 
-
+def news(request):
+    return render(request, 'manageevents/news.html', {'title': 'Новости'})
