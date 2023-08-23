bc65868766550a33f6c1d994d14d5f35d64fcfba
Andrey <andreypremiere@mail.ru>
2023-08-23 19:21:32 +0700

    Частично настроена админ-панель. Добавлена модель для мероприятий

--- posterSFU/manageevents/admin.py
+++ posterSFU/manageevents/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import ListUniversity, Events
 
-# Register your models here.
+
+class EventsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'date_of_event', 'time_of_event')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'organizer')
+
+admin.site.register(Events, EventsAdmin)
--- posterSFU/manageevents/models.py
+++ posterSFU/manageevents/models.py
@@ -7,3 +7,34 @@ class ListUniversity(models.Model):
     """
     # id формируется автоматически
     title = models.CharField(max_length=20)
+
+
+    def __str__(self):
+        return self.title
+
+
+class Events(models.Model):
+    """
+    Класс для формирования мероприятий
+    """
+    title = models.CharField(max_length=50, verbose_name='Название')
+    date_of_event = models.DateField(auto_now_add=False, auto_now=False, verbose_name='Дата мероприятия')
+    time_of_event = models.TimeField(auto_now=False, auto_now_add=False, verbose_name='Время мероприятия')
+    description = models.TextField(max_length=255, verbose_name='Описание')
+    preview = models.ImageField(upload_to="preview/%Y/%m/", verbose_name='Превью')
+    need_team = models.BooleanField(verbose_name='Необходимость команды',)
+    quantity_of_participant = models.IntegerField(verbose_name='Количество участников',)
+    all_free_place = models.IntegerField(verbose_name='Свободные места',)
+    place_of_meeting = models.CharField(max_length=100, verbose_name='Место встречи')
+    organizer = models.ForeignKey('ListUniversity', on_delete=models.PROTECT, verbose_name='Организатор')
+
+
+    def __str__(self):
+        return self.title
+
+
+    # Специальный класс для отображения в админ-панели
+    class Meta:
+        verbose_name = 'Мероприятия'
+        verbose_name_plural = 'Мероприятия'
+        ordering = ['date_of_event', 'time_of_event']
